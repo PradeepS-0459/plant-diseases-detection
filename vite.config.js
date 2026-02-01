5afa4ac395d30d47e3cefb4995ccdf02a56e4b98
Pradeep Sasidharan <pradeepsasidharank19592004@gmail.com>
2026-02-01 19:15:09 +0530

    Updated the config file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/plant-diseases-detection/",   // 👈 GitHub repo name
   plugins: [react()],
   server: {
     proxy: {
